b9da006c9504002b288a91616e5571660f68b12b
Pyradi <huwer.marco@gmail.com>
2026-04-30 18:31:56 +0200

    Update route.js

--- api/route.js
+++ api/route.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
     }
 
     const response = await fetch(
-      "https://api.heigit.org/v2/directions/foot-walking",
+      "https://api.openrouteservice.org/v2/directions/foot-walking",
       {
         method: "POST",
         headers: {
